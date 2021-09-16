b7f1c023b1cb71f003c1de5e9269f4ae0a7ded7b
rgferrari <renegferrari@gmail.com>
2021-09-16 15:48:18 -0300

    Create repository

---
+++ README.md
@@ -0,0 +1,9 @@
+# Quadtree
+
+<p align="center">
+  <img width="70%" alt="sample" src="Figure_1.png">
+</p>
+
+Quadtree desenvolvida em python.
+
+Para executá-la basta executar o arquivo [quatree.py]()
---
+++ quadtree.py
@@ -0,0 +1,96 @@
+import random
+import matplotlib.pyplot as plt
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Rectangle():
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def contains(self, point):
+        return (point.x < self.x + self.w and
+                point.x > self.x - self.w and
+                point.y < self.y + self.h and
+                point.y > self.y - self.h)
+
+class QuadTree():
+    def __init__(self, boundary, capacity):
+        self.boundary = boundary
+        self.capacity = capacity
+        self.points = []
+        self.is_divided = False
+
+    def subdivide(self):
+        x = self.boundary.x
+        y = self.boundary.y
+        w = self.boundary.w
+        h = self.boundary.h
+
+        nw = Rectangle(x - w/2, y - w/2, w/2, h/2)
+        self.northwest = QuadTree(nw, self.capacity)
+        ne = Rectangle(x + w/2, y - h/2, w/2, h/2)
+        self.northeast = QuadTree(ne, self.capacity)
+        sw = Rectangle(x - w/2, y + w/2, w/2, h/2)
+        self.southwest = QuadTree(sw, self.capacity)
+        se = Rectangle(x + w/2, y + w/2, w/2, h/2)
+        self.southeast = QuadTree(se, self.capacity)
+
+        self.is_divided = True
+
+
+    def insert(self, point):
+        if(not self.boundary.contains(point)):
+            return
+
+        if(len(self.points) < self.capacity):
+            self.points.append(point)
+        else:
+            if(not self.is_divided):
+                self.subdivide()
+                self.is_divided = True
+
+            self.northwest.insert(point)
+            self.northeast.insert(point)
+            self.southwest.insert(point)
+            self.southeast.insert(point)
+
+    def draw(self):
+        x = [self.boundary.x - self.boundary.w, self.boundary.x + self.boundary.w] 
+        y = [self.boundary.y - self.boundary.h, self.boundary.y + self.boundary.h]
+
+        plt.plot(x, [0,0], 'k-') 
+        plt.plot(x, [self.boundary.y + self.boundary.h, self.boundary.y + self.boundary.h], 'k-') 
+        plt.plot([0,0], y, 'k-')
+        plt.plot([self.boundary.x + self.boundary.w, self.boundary.x + self.boundary.w], y, 'k-')
+
+        for p in self.points:
+            plt.plot(p.x, p.y, 'k.')
+
+        if(self.is_divided):
+            self.northwest.draw()
+            self.northeast.draw()
+            self.southwest.draw()
+            self.southeast.draw() 
+
+def main():
+    boundary = Rectangle(200,200,200,200)
+    quadtree = QuadTree(boundary, 4)
+
+    width = 400
+    height = 400
+
+    for _ in range(500):
+        p = Point(random.randrange(width), random.randrange(height))
+        quadtree.insert(p)
+
+    quadtree.draw()
+
+    plt.show()
+
+main()   
